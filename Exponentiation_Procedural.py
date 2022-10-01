4e8f7be905ece07f0f800b3b5f62774128f2da0e
ravalivemula8 <113603361+ravalivemula8@users.noreply.github.com>
2022-10-01 18:02:24 +1000

    Arithmetic Operations - 10.01.2022

---
+++ Exponentiation_Procedural.py
@@ -0,0 +1,17 @@
+#step 1  Get the First number
+x = 15
+#Step 2  Get the second number
+y = 10
+#Step 3  Add two numbers and store results
+z = x**y
+#step 4
+print(z)
+
+
+x = 5
+#Step 2  Get the second number
+y = 6
+#Step 3  Add two numbers and store results
+z = x**y
+#step 4
+print(z)
---
+++ Sub_Procedural.py
@@ -0,0 +1,17 @@
+#step 1  Get the First number
+x = 25
+#Step 2  Get the second number
+y = 50
+#Step 3  Add two numbers and store results
+z = x-y
+#step 4
+print(z)
+
+
+x = 40
+#Step 2  Get the second number
+y = 25
+#Step 3  Add two numbers and store results
+z = x-y
+#step 4
+print(z)
