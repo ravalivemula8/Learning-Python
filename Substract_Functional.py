18e7cc568c9ee8d9878e2f7ed3eeeb215abbb49d
ravalivemula8 <113603361+ravalivemula8@users.noreply.github.com>
2022-10-12 19:20:54 +1100

    Arithmetic Operations - 10.06.2022

---
+++ Substract_Functional.py
@@ -0,0 +1,12 @@
+
+# get the first number
+# print(*enter first input*)
+x = input("enter first input")
+print("first value is" + x)
+print("enter second input")
+y = input("enter second input")
+print("second value is" + y)
+# add the numbers and store the results
+print("results")
+z = (int(x) - int(y))
+print(z)
