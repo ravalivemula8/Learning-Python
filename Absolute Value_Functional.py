58535ef43b6dc2a4f9077ea3bd38764a1daa7102
ravalivemula8 <113603361+ravalivemula8@users.noreply.github.com>
2022-10-13 19:33:05 +1100

    Arithmetic Operations - 10.06.2022

--- Absolute Value_Functional.py
+++ Absolute Value_Functional.py
@@ -1,15 +1,14 @@
 # get the first number
-# get the first number
-# print(*enter first input*)
+# print(enter first input)
 x = input("enter first input")
 print("first value is" + x)
 print("enter second input")
 y = input("enter second input")
 print("second value is" + y)
-# add the numbers and store the results
+# sub the numbers and store the results
 print("results")
-z = (int(x) + int(y))
-if z<0:
-    print(-Z)
+z = (int(x) - int(y))
+if z < 0:
+    print(-z)
 else:
     print(z)
--- Exponential_Functional.py
+++ Exponential_Functional.py
@@ -1,12 +1,23 @@
-# get the first number
-# get the first number
-# print(*enter first input*)
-x = input("enter first input")
-print("first value is" + x)
-print("enter second input")
-y = input("enter second input")
-print("second value is" + y)
-# add the numbers and store the results
-print("results")
-z = (int(x) ** int(y))
-print(z)
+def divide(x, y):
+    divide= int(p) / int(q)
+    print(divide)
+
+
+print("first input")
+a = input()
+if int(a) < 0:
+    print("not valid")
+    exit()
+else:
+    print("first number is " + a)
+print("second input")
+b = input()
+if int(b) < 0:
+    print("not valid ")
+    exit()
+else:
+    print("second number is " + b)
+    print("result")
+divide = int(a) / int(b)
+
+print(divide)
